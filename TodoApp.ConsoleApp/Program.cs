12b732b5d7c16f66decda965234e7d58e12a958b
Redsart <redsartvimes@yahoo.com>
2019-10-23 19:40:09 +0300

    Change the main class name, add a TodoService class for all the CRUD operations.

---
+++ TodoApp.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TodoApp.Services;
+
+namespace TodoApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this program is making a task's and save them to xml document. Enjoy :)");
+            Console.WriteLine();
+            
+             TodoServices.ReadOrWrite();
+        }
+    }
+}
